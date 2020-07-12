ba25077036b39fa5a9543739f3afd31ad9a4b13c
James Schnebly <jameschnebly@gmail.com>
2020-07-11 18:49:59 -0700

    Profile API's complete with the exception of github repos due to github OAuth deprecation -- todo=fix that route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 // imports
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const axios = require('axios');
+const config = require('config');
 
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -211,6 +213,127 @@ router.put('/experience', [auth, [
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    DELETE profile experience
+// @access  Private
+router.delete('/experience/:exp_id', auth,  async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // get remove index and remove exp
+        const removeIdx = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIdx, 1);
+
+        // save document and return response
+        await profile.save();
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put('/education', [auth, [
+    check('school', 'School is required').not().isEmpty(),
+    check('degree', 'Degree is required').not().isEmpty(),
+    check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.education.unshift(newEdu);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    DELETE profile education
+// @access  Private
+router.delete('/education/:edu_id', auth,  async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // get remove index and remove exp
+        const removeIdx = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIdx, 1);
+
+        // save document and return response
+        await profile.save();
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos
+// @access  Pblic
+// @todo    Fix github api authentication
+router.get('/github/:username', (req ,res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`
+        };
+
+        axios.get(options.uri).then((response) => {
+            if(response.status !== 200){
+                return res.status(404).json({msg: 'No Github profile found'});
+            }
+
+            return res.json(JSON.parse(response.data));
+
+        }, (error) => {
+            console.error(error.message);
+            return res.status(500).send('Error getting github repos');
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send('Server Error');
     }
 });
 
